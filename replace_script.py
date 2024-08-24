a8b67d5dd74b4b866fb1d34554c3398ada235817
yozhikov13 <vlad.me97@gmail.com>
2024-08-24 18:39:41 +0400

    pre_final
    added line correction with character replacement, writing the final result to a text file and outputting the result to the console

--- replace_script.py
+++ replace_script.py
@@ -9,6 +9,30 @@ def read_conf(read_file):
 
     return replace_dict
 
+def correction_lines(read_file, replacements):
+    edited_lines = []
+    with open(read_file, 'r') as file:
+        for line in file:
+            correct_line, count_rep = change_line(line, replacements)
+            edited_lines.append((correct_line, count_rep))
+
+    edited_lines.sort(key=lambda x: x[1], reverse=True)
+    correct_lines = [line for line, _ in edited_lines]
+
+    return correct_lines
+
+def change_line(line, replacements):
+    count_rep = 0
+    line = line.strip()
+    replace_line = line
+
+    for key, value in replacements.items():
+        count = line.count(key)
+        replace_line = replace_line.replace(key, value)
+        count_rep += count * len(key)
+
+    return replace_line, count_rep
+
 def main():
     if len(sys.argv) != 3:
         print("Error! Print: python replace_script.py 'configuration_file' 'text_file'")
@@ -18,6 +42,12 @@ def main():
     text_file = sys.argv[2]
 
     replacements = read_conf(conf_file)
+    edited_lines = correction_lines(text_file,replacements)
+
+    with open(text_file, 'w') as file:
+        for line in edited_lines:
+            print(line)
+            file.write(line + '\n')
 
 
 if __name__ == "__main__":
